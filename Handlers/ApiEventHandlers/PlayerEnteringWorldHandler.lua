aed8f95108642d33755c10ee9be1a329ce98957d
Jacob Cacciamani <cacciamani10@gmail.com>
2025-02-12 17:22:44 -0500

    Fix filename

--- Handlers/ApiEventHandlers/PlayerEnteringWorldHandler.lua
+++ Handlers/ApiEventHandlers/PlayerEnteringWorldHandler.lua
@@ -1,8 +1,8 @@
 _G.HCT_Handlers = _G.HCT_Handlers or {}
 
-_G.HCT_Handlers.OnPlayerEnteringWorldHandler = {
+_G.HCT_Handlers.PlayerEnteringWorldHandler = {
     GetEventType = function() return "PLAYER_ENTERING_WORLD" end,
-    GetHandlerName = function() return "OnPlayerEnteringWorldHandler" end,
+    GetHandlerName = function() return "PlayerEnteringWorldHandler" end,
 
     HandleEvent = function(self, HCT, event)
         -- Your event handling logic here
