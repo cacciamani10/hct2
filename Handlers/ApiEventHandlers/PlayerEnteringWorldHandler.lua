33a37cb2d68333b172c03f982a413b6e35a6e26d
Jacob Cacciamani <cacciamani10@gmail.com>
2025-02-13 14:20:04 -0500

    new character detected

--- Handlers/ApiEventHandlers/PlayerEnteringWorldHandler.lua
+++ Handlers/ApiEventHandlers/PlayerEnteringWorldHandler.lua
@@ -6,6 +6,10 @@ _G.HCT_Handlers.PlayerEnteringWorldHandler = {
 
     HandleEvent = function(self, HCT, event)
         if not HCT then return end
+        local xp = UnitXP("player") 
+        if xp == 0 then
+            HCT:Print("PlayerEnteringWorldHandler: New character detected.") 
+        end
         -- Request data
         HCT_Broadcaster:RequestContestData() -- Request contest data from the guild.
         HCT:Print("Player entering world event handled.")
