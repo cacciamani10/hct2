104a416cd6d1f2acc47ae22eb760405710d1c858
Jacob Cacciamani <cacciamani10@gmail.com>
2025-02-14 16:49:27 -0500

    Save new zones

--- Handlers/ApiEventHandlers/ZoneChangedNewAreaHandler.lua
+++ Handlers/ApiEventHandlers/ZoneChangedNewAreaHandler.lua
@@ -1,5 +1,7 @@
 _G.HCT_Handlers = _G.HCT_Handlers or {}
 
+local function GetDB() return _G.HCT_Env.GetAddon().db.profile end
+
 -- Fires when the player enters a zone.
 _G.HCT_Handlers.ZoneChangedNewAreaHandler = {
 
@@ -16,6 +18,16 @@ _G.HCT_Handlers.ZoneChangedNewAreaHandler = {
             local mapInfo = mapId and C_Map.GetMapInfo(mapId)
             local mapName = mapInfo and mapInfo.name or "Unknown"
             HCT:Print("ZoneChangedNewAreaHandler: Entered new area: " .. zone .. " - " .. subzone .. " - " .. mapName)
+            local db = GetDB()
+            db.localAchievementProgressData = db.localAchievementProgressData or {}
+            local charKey = HCT_DataModule:GetCharacterKey()
+            db.localAchievementProgressData[charKey] = db.localAchievementProgressData[charKey] or {}
+            db.localAchievementProgressData[charKey].zonesVisited = db.localAchievementProgressData[charKey].zonesVisited or {}
+            db.localAchievementProgressData[charKey].zonesVisited[zone] = db.localAchievementProgressData[charKey].zonesVisited[zone] or {}
+            if not db.localAchievementProgressData[charKey].zonesVisited[zone][subzone] then
+                HCT:Print("New zone discovered: " .. zone .. " - " .. subzone)
+                db.localAchievementProgressData[charKey].zonesVisited[zone][subzone] = true
+            end
         end)
     end,
 }
