ebb73da6d80155cd69f3e8f195403736b596607b
Jacob Cacciamani <cacciamani10@gmail.com>
2025-02-14 09:51:20 -0500

    Fix skill handler not matching to right params

--- Handlers/ApiEventHandlers/ChatMsgSkillHandler.lua
+++ Handlers/ApiEventHandlers/ChatMsgSkillHandler.lua
@@ -9,14 +9,25 @@ _G.HCT_Handlers.ChatMsgSkillHandler = {
         return "ChatMsgSkillHandler"
     end,
 
-    HandleEvent = function(self, HCT, event)
-         --  arg1 your skill in mining has increased to 57
+    HandleEvent = function(self, HCT, eventName, text, playerName, languageName, channelName, playerName2, specialFlags,
+                           zoneChannelID, channelIndex, channelBaseName, languageID, lineID, guid, bnSenderID, isMobile,
+                           isSubtitle, hideSenderInLetterbox, supressRaidIcons)
         if HCT then
-
-            local skillName, skillLevel = string.match(event, "your skill in (.+) has increased to (%d+)")
+            local skillThresholds = { 75, 150, 225, 300 }
+            local lowerText = text:lower():gsub("%.$", "")
+            local skillName, skillLevel = string.match(lowerText, "your skill in%s+([%a ]+)%s+has increased to%s+(%d+)")
             if skillName and skillLevel then
                 local charKey = HCT_DataModule:GetCharacterKey()
-                HCT_DataModule:CheckProfessionAchievement(charKey, skillName, skillLevel)
+                local skillLevelNumber = tonumber(skillLevel)
+                if skillLevelNumber then
+                    if tContains(skillThresholds, skillLevelNumber) then
+                        HCT:Print("Skill threshold detected. Checking achievement for " ..
+                        skillName .. " to " .. skillLevel)
+                        HCT_DataModule:CheckProfessionAchievement(charKey, skillName, skillLevelNumber)
+                    end
+                end
+            else
+                HCT:Print("No skill match in: " .. text)
             end
         end
     end
