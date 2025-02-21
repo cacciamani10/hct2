42b0ce001dddfdd123ead8b1fd773d6024485d5f
gglasgow <geraldjglasgow@gmail.com>
2025-02-21 13:03:49 -0500

    adding another feat

---
+++ Handlers/ApiEventHandlers/ChatMsgCombatMiscInfoHandler.lua
@@ -0,0 +1,22 @@
+_G.HCT_Handlers = _G.HCT_Handlers or {}
+
+_G.HCT_Handlers.ChatMsgCombatMiscInfoHandler = {
+    GetEventType = function()
+        return "CHAT_MSG_COMBAT_MISC_INFO"
+    end,
+
+    GetHandlerName = function()
+        return "ChatMsgCombatMiscInfoHandler"
+    end,
+
+    HandleEvent = function(self, HCT, eventName, text)
+        local playerName = UnitName("player")
+        local playerLevel = UnitLevel("player")
+        if playerLevel > 10 and text then
+            if text:find(playerName .. " fell to his death") or text:find(playerName .. " fell to her death") then
+                print("Falling death detected for " .. playerName)
+                _G.ACHIEVEMENTS.Achievement_Feats:CheckAchievement(513)
+            end
+        end
+    end
+}
